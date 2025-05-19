8229e5ce2b2d89e3e61b383502137b505903c999
Tiago <tiagojesusilveira@gmail.com>
2025-05-19 17:35:26 +0200

    Fix is_int bug to allow all int values

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,10 +1,20 @@
 #include "../includes/libft.h"
 
-int	is_number(char *num)
+int	is_int(char *num)
 {
+	long int	final_num;
+	int			s;
+
+	final_num = 0;
+	s = 0;
+	if (*(num++) == '-')
+		s++;
 	while (*num)
 	{
 		if (*num < '0' || *num > '9')
+			return (0);
+		final_num = final_num * 10 + *num - '0';
+		if ((final_num > 2147483648 && s) || (final_num > INT_MAX && !s))
 			return (0);
 		num++;
 	}
@@ -18,7 +28,7 @@ int	*data_loader(int argc, char *argv[])
 	temp = malloc(sizeof(int) * (argc - 1));
 	while (--argc > 0)
 	{
-		if (!is_number(argv[argc]))
+		if (!is_int(argv[argc]))
 		{
 			return (NULL);
 			free(temp);
